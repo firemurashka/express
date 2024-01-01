2435c8410f32ce448af63fd1de6ea4fd40d840a7
firemurashka <firemurashka@gmail.com>
2024-01-01 20:41:16 +0300

    use

--- myapp/app.js
+++ myapp/app.js
@@ -1,12 +1,16 @@
 var express = require('express');
 var app = express();
-const port=3000;
-app.get('/', function (req, res) {
-	res.send('Hello World Vika!');
+const port = 3000;
+
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+	console.log('The request body JSON is converrted to a JS object.');
 });
 
-app.listen(port, function () {
-	console.log(`Example app listening on port ${port}!`);
+app.listen(port, () => {
+	console.log(`The server is listening on port ${port}!`);
 });
 
 //node app.js
