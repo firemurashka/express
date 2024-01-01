3d1c50f3aee9631a9497c4c94a30b30b763e5a22
firemurashka <firemurashka@gmail.com>
2024-01-01 20:50:39 +0300

    authenticate

--- myapp/app.js
+++ myapp/app.js
@@ -2,15 +2,21 @@ var express = require('express');
 var app = express();
 const port = 3000;
 
-
 app.use(express.json());
 
-app.get('/', (req, res) => {
-	console.log('The request body JSON is converrted to a JS object.');
-});
+function authenticate(req, res) {
+	if (req.body.role === 'Admin') {
+		console.log('Authenticated');
+	} else {
+		console.log('Unauthotized');
+	}
+}
 
+app.use(authenticate);
 app.listen(port, () => {
 	console.log(`The server is listening on port ${port}!`);
 });
 
 //node app.js
+//The server is listening on port 3000!
+//The request body JSON is converrted to a JS object.
