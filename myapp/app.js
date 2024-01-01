9458fc970cc41bf6280dfe4fa26688ff8730b1e2
firemurashka <firemurashka@gmail.com>
2024-01-01 21:08:15 +0300

    get use

--- myapp/app.js
+++ myapp/app.js
@@ -1,19 +1,33 @@
 const express = require('express');
-const morgan = require('morgan');
+//const morgan = require('morgan');
 const app = express();
-
 const port = 3000;
 
-app.use(morgan('tiny'));
+app.use((req, res) => {
+	console.log('Incoming request…');
+	console.log('Request type: ' + req.method);
+	console.log('The flow will stop here.');
+});
 
 app.get('/', (req, res) => {
-	res.status(500).send('Request received');
+	console.log("Did we make it to the `get()` middleware step?");
 });
 
 app.listen(port, () => {
 	console.log(`The server is listening on port ${port}!`);
 });
 
+
+
+//---------------------------------------------------------------------
+//?--Запуск сервера:
 //node app.js
+
+//?--Терминал:
 //The server is listening on port 3000!
-//The request body JSON is converrted to a JS object.
+//Incoming request…
+//Request type: GET
+//The flow will stop here.
+
+//?--Браузер (http://localhost:3000/):
+//
